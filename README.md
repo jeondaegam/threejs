6aaada5fa4bc29b9c27d557a2260dbd78780dde2
jeondaegam <jeondaegam@naver.com>
2021-12-25 23:47:33 +0900

    scene, readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # threejs
 lets begin threejs
 
+# threejs.org 
+<https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene>
+
 ### 지켜야 할 것!
 - 모든 파일의 버전을 동일하게 맞추는 것이 무엇보다 중요하다.
 - 서로 다른 버전에서 import 하거나, 다른 버전의 예제를 사용하지 마세요.
@@ -10,6 +13,31 @@ Web Graphics Library의 약자로
 웹에서 2D 및 3D를 렌더링하기위한 JavaScript API
 
 
-### Scene 만들기
+### three.js import
+doc 예시와 다르게 경로를 끝까지 작성해야했음.
+```javascript
+import * as THREE from '../node_modules/three/build/three.module.js';
+// import * as THREE from 'three';
+```
+html에 type="module" 추가
+
+```html
+
+<script type="module" src="js/scene.js"></script>
+```
+
+
+### Creating a Scene
 three.js로 무언가를 표현하려면 scene, camera 그리고 renderer가 필요합니다.   
 이를 통해 카메라로 장면을 구현할 수 있습니다.
+```javascript
+const camera = new THREE.PerspectiveCamera(90, // => 각도
+window.innerWidth / window.innerHeight, 0.1, 1000);
+THREE.PerspectiveCamera(시야각(FOV), )
+```
+1. FOV(Field of view, 시야각) : 각도값. 시점의 화면이 보여지는 정도
+2. aspect ratio(종횡비) : 스크린 높이와 너비의 비율 ?
+3. near과 far 절단면 : 원근감을 표현한다. 
+   - far보다 멀리 있거나 near보다 가까이 있으면 렌더링 되지 않는다.
+
+
--- js/scene.js
+++ js/scene.js
@@ -53,35 +53,38 @@ scene.add( line );
 renderer.render( scene, camera );*/
 
 // Scene 만들기
-// 쉽게 말해 카메라로 찍고있는 장면을 설정하는 것!
-// 어느 각도에서 보고있는지, 화면 크기에 대한 사물의 비율, 원근감등을 표현할 canvas를 만든다.
+// 카메라에 담을 장면을 설정하는 것.
+// 어느 각도(시야)에서 보고있는지, 화면 크기에 대한 사물의 비율, 원근감등을 표현할 canvas를 만든다.
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,
+const camera = new THREE.PerspectiveCamera(90, // => 각도
     window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight); // 렌더링 할 곳의 사이즈 설정
 document.body.appendChild(renderer.domElement);
 
 // 큐브
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const geometry = new THREE.BoxGeometry(1,2,1); // 모양이 변한다!!
+const material = new THREE.MeshBasicMaterial({color: "blue"});
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 5; // 거리조절
 
-// scene 렌더링 : 화면이 새로고침 될 때마다 렌더링 해준다. (일반적으로 1초에 60번 렌더링)
+// scene 렌더링
+// 화면이 새로고침 될 때마다 렌더링 해준다. (일반적으로 1초에 60번 렌더링)
 // setInterval 을 쓰지 않는 이유 ? 유저가 브라우저 창에서 이탈했을 때 멈춰주는 기능 등등 이점이 많다.
 function animate() {
   requestAnimationFrame(animate);
 
   // 큐브 애니메이팅 : 큐브를 회전시킨다 !!
+  // 숫자가 커질 수록 speed up, -를 붙이면 반대방향
   cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.y += -0.01;
   renderer.render(scene, camera);
 }
+
 animate();
 
 
